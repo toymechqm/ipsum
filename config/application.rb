703089a296b2a899baabd28d3a876817135fd0dc
Josh <toyjtoy@gmail.com>
2016-01-01 22:38:53 -0500

    test application rb file

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Ipsum
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    #ADD Fot IMAGES ON HEROKU
+    config.serve_static_assets = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
