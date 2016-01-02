698110b5450d4783403c9596579c27dd3f8634f2
Josh <toyjtoy@gmail.com>
2016-01-01 22:52:10 -0500

    testing for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # ADD FROM STACK OVERFLOW FOR HEROKU ASSETS
-  config.serve_static_files = true
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
