c611c005fd6da8fec52c494cb4a49664199fc264
Josh <toyjtoy@gmail.com>
2016-01-05 16:44:57 -0500

    View Changes

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151227211121) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "bio"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
